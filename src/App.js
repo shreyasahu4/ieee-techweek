23562279e8ead08b273d79b02128c0a940a2fc88
sherriaa <shreyasahu5256@gmail.com>
2022-01-14 14:07:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Timeline from "./Components/Timeline/Timeline"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <Timeline/>
         </p>
         <a
           className="App-link"
---
+++ src/Components/Timeline/TimelineData.js
@@ -0,0 +1,45 @@
+// import google from "./images/google.png";
+// import jpm from "./images/jpm.png";
+// import microsoft from "./images/microsoft.png";
+// import deshaw from "./images/deshaw.png";
+// import apple from "./images/apple.png";
+
+export const data = [
+  {
+    heading: "Google Genration Scholarship",
+    description: " Women students(pursuing computer)if selected students will receive $1,000 USD",
+    // img: google ,
+    date: " Dec 17- Jan 4",
+  },
+  {
+    heading: " DeShaw",
+    
+    // img: deshaw,
+    date: "30 August",
+  },
+  {
+    heading: "STEP",
+
+    //img: google,
+    date: "Multiple Dates",
+  },
+  {
+    heading: "JPMorgan Quant Mentorship",
+  
+    //img: jpm,
+    date: "16 Feb",
+  },
+  {
+    heading: "thing 7",
+   
+    //img: microsoft,
+    date: "Feb",
+  },
+
+  {
+    heading: "thing 8 ",
+   
+    //img: apple,
+    date: " March",
+  },
+];
